3d197e277a8738de8a9060f923df4ee0bae82222
Adi Gosto <adigosto88@gmail.com>
2024-11-10 19:21:39 +0100

    Adding CORS support in the API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -12,13 +12,13 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
   title = 'DatingApp';
-  users: any;
+  users : any;
 
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/users').subscribe({
-      next: Response => this.users = Response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
+      next : response => this.users = response,
+      error : error => console.log(error),
+      complete : () => console.log('Request has completed')
     })
   }
 }
